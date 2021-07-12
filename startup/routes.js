d8dd1a6a2d607533f04746951bd4b26de99b19de
Joseph Greene <josephadamgreene@gmail.com>
2021-07-12 02:12:35 -0400

    Added cors

--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const bots = require('../routes/bots');
 const users = require('../routes/users');
+const auth = require('../routes/auth');
 
 module.exports = function(app) {
+    app.use(cors());
     app.use(express.json());
     app.use('/api/bots', bots);
     app.use('/api/users', users);
+    app.use('/api/auth', auth);
 }
