d65b04daf3bbca6e1349b3d95407d49f5f36bb49
Joseph Greene <josephadamgreene@gmail.com>
2021-07-20 02:00:53 -0400

    Only bots with status 'true' are added to active botClients

--- startup/discordBot.js
+++ startup/discordBot.js
@@ -2,7 +2,7 @@ const {Bot} = require("../models/bot");
 const {initiateBot} = require("../discordBot/botClientUtils");
 
 module.exports = async () => {
-	const bots = await Bot.find();
+	const bots = await Bot.find({status: true});
 
 	bots.forEach((bot) => {
 		initiateBot(bot);
