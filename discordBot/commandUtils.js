ae6b597c09b9e050ecd82aa97ffca3a1337c2754
Joseph Greene <josephadamgreene@gmail.com>
2021-08-03 07:44:18 -0400

    add command utilities

---
+++ discordBot/commandUtils.js
@@ -0,0 +1,18 @@
+// Returns the name actually displayed in the server for the user
+async function getAuthorDisplayName (msg) {
+  const member = await msg.guild.member(msg.author);
+  return member.nickname ? member.nickname : msg.author.username;
+}
+
+async function messageParser (message, botModuleResponse) {
+  let parsedMessage = botModuleResponse;
+
+  parsedMessage = parsedMessage
+                    .replace(/{user}/g, message.author.username)
+                    .replace(/{user_display}/g, await getAuthorDisplayName(message))
+                    .replace(/{channel}/g, message.channel.name);
+
+  return parsedMessage;
+}
+
+exports.messageParser = messageParser;
