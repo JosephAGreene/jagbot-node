c4f6792127308b5f2a8bc2acde28371b54ee3d12
Joseph Greene <josephadamgreene@gmail.com>
2021-09-30 00:39:55 -0400

    add update join announcement route

--- routes/announcementModules.js
+++ routes/announcementModules.js
@@ -5,7 +5,7 @@ const { initiateBot } = require("../discordBot/botClientUtils");
 const { Bot } = require("../models/bot");
 const { Announcement } = require("../models/announcement");
 
-router.post("/new-announcement", async (req, res) => { 
+router.post("/new-announcement", async (req, res) => {
   const bot = await Bot.findById(req.body.botId);
 
   // Return 409 if announcement type already exists 
@@ -13,19 +13,19 @@ router.post("/new-announcement", async (req, res) => {
   let serverCheck = false;
   const reqType = req.body.type;
   const reqServer = req.body.responseChannel.serverId;
-  for(let i=0; i < bot.announcementModules.length; i++) {
+  for (let i = 0; i < bot.announcementModules.length; i++) {
     let type = bot.announcementModules[i].type;
     let server = bot.announcementModules[i].responseChannel.serverId;
-    if(server === reqServer && type === reqType) {
+    if (server === reqServer && type === reqType) {
       serverCheck = true;
       break;
     }
   }
-  if(serverCheck) {
+  if (serverCheck) {
     return res.status(409).send("duplicate server");
   }
 
-  const newAnnouncement = new Announcement ({
+  const newAnnouncement = new Announcement({
     type: req.body.type,
     responseChannel: req.body.responseChannel,
     responseType: req.body.responseType,
@@ -42,6 +42,68 @@ router.post("/new-announcement", async (req, res) => {
   });
 
   bot.announcementModules.push(newAnnouncement);
+
+  await bot.save();
+
+  initiateBot(bot);
+
+  res.send(bot);
+});
+
+router.put("/update-announcement", async (req, res) => {
+  const bot = await Bot.findById(req.body.botId);
+
+  // If module exists, set moduleLocation to the module's index number
+  let moduleLocation = -1;
+  for (let i = 0; i < bot.announcementModules.length; i++) {
+    if (String(bot.announcementModules[i]._id) === String(req.body.moduleId)) {
+      moduleLocation = i;
+      break;
+    }
+  }
+
+  // If module doesn't exist, return 404
+  if (moduleLocation < 0) {
+    return res.sendStatus(404);
+  }
+
+  // Return 409 if announcement type already exists 
+  // for the given serverId, ignoring whatever value already 
+  // exists for the current module
+  let serverCheck = false;
+  const reqType = req.body.type;
+  const reqServer = req.body.responseChannel.serverId;
+  for (let i = 0; i < bot.announcementModules.length; i++) {
+    if (String(bot.announcementModules[i]._id) !== String(req.body.moduleId)) {
+      let type = bot.announcementModules[i].type;
+      let server = bot.announcementModules[i].responseChannel.serverId;
+      if (server === reqServer && type === reqType) {
+        serverCheck = true;
+        break;
+      }
+    }
+  }
+  if (serverCheck) {
+    return res.status(409).send("duplicate server");
+  }
+
+  // Update module with new information
+  bot.announcementModules.splice(moduleLocation, 1, {
+    ...bot.announcementModules[moduleLocation],
+    type: req.body.type,
+    responseChannel: req.body.responseChannel,
+    responseType: req.body.responseType,
+    response: req.body.response,
+    embedTitle: req.body.embedTitle,
+    embedLinkURL: req.body.embedLinkURL,
+    embedColor: req.body.embedColor,
+    embedThumbnailURL: req.body.embedThumbnailURL,
+    embedMainImageURL: req.body.embedMainImageURL,
+    embedDescription: req.body.embedDescription,
+    embedFields: req.body.embedFields,
+    embedFooter: req.body.embedFooter,
+    embedFooterThumbnailURL: req.body.embedFooterThumbnailURL,
+  });
 
   await bot.save();
 
