f362c14c84e798ce670ca031a88ed03d09a42db9
Joseph Greene <josephadamgreene@gmail.com>
2021-10-01 01:21:52 -0400

    Fix getAuthorDisplayName bug when member isn't in server

--- discordBot/commandUtils.js
+++ discordBot/commandUtils.js
@@ -3,20 +3,20 @@ const { returnChannelObject } = require("./botClientUtils");
 
 // Determines if botmodule should respond with a basic text message or embeded,
 // then returns an executable function will send the proper response
-async function buildResponse (message, botModule) {
-  if(botModule.responseType === "basic") {
+async function buildResponse(message, botModule) {
+  if (botModule.responseType === "basic") {
     const basicResponse = await messageParser(message, botModule.response);
     return () => message.channel.send(basicResponse.toString());
   } else if (botModule.responseType === "embed") {
     const embedResponse = await buildEmbedResponse(message, botModule);
-    return () => message.channel.send({embeds: [embedResponse]});
+    return () => message.channel.send({ embeds: [embedResponse] });
   }
 }
 
 // Determines if announcement should respond with a basic text message or embeded,
 // then returns an executable function will send the proper response to the desired
 // server channel
-async function buildAnnouncement (botId, announcement, member) {
+async function buildAnnouncement(botId, announcement, member) {
   // get channel object, as it's required in order to access the send method
   const channel = await returnChannelObject(botId, announcement.responseChannel.channelId);
   // Recreate a psuedo message object with necessary properties that are normally
@@ -25,34 +25,34 @@ async function buildAnnouncement (botId, announcement, member) {
   // necessary methods to both make this module work AND make it integrate with the 
   // already functioning commandUtil functions
   const messageObject = {
-    channel : channel,
+    channel: channel,
     guild: channel.guild,
     author: member.user
   }
 
-  if(announcement.responseType === "basic") {
+  if (announcement.responseType === "basic") {
     const basicResponse = await messageParser(messageObject, announcement.response);
     return () => messageObject.channel.send(basicResponse.toString());
   } else if (announcement.responseType === "embed") {
     const embedResponse = await buildEmbedResponse(messageObject, announcement);
-    return () => messageObject.channel.send({embeds: [embedResponse]});
+    return () => messageObject.channel.send({ embeds: [embedResponse] });
   }
 }
 
 // Builds and returns a discord embed object
-async function buildEmbedResponse (message, botModule) {
+async function buildEmbedResponse(message, botModule) {
   // Assume embedTitle always exists, as the database validation requires
   const embedResponse = new MessageEmbed().setTitle(botModule.embedTitle);
 
-  if(botModule.embedLinkURL) { embedResponse.setURL(botModule.embedLinkURL); }
+  if (botModule.embedLinkURL) { embedResponse.setURL(botModule.embedLinkURL); }
 
-  if(botModule.embedColor) { embedResponse.setColor(botModule.embedColor); } 
+  if (botModule.embedColor) { embedResponse.setColor(botModule.embedColor); }
 
-  if(botModule.embedThumbnailURL) { embedResponse.setThumbnail(botModule.embedThumbnailURL); }
+  if (botModule.embedThumbnailURL) { embedResponse.setThumbnail(botModule.embedThumbnailURL); }
 
-  if(botModule.embedMainImageURL) { embedResponse.setImage(botModule.embedMainImageURL); }
+  if (botModule.embedMainImageURL) { embedResponse.setImage(botModule.embedMainImageURL); }
 
-  if(botModule.embedDescription) {
+  if (botModule.embedDescription) {
     const basicResponse = await messageParser(message, botModule.embedDescription);
     embedResponse.setDescription(basicResponse.toString());
   }
@@ -62,11 +62,11 @@ async function buildEmbedResponse (message, botModule) {
     embedResponse.addField(field.name, field.value, field.inline);
   });
 
-  if(botModule.embedFooter) {
-    if(botModule.embedFooterThumbnailURL) {
-      embedResponse.setFooter(botModule.embedFooter, botModule.embedFooterThumbnailURL); 
+  if (botModule.embedFooter) {
+    if (botModule.embedFooterThumbnailURL) {
+      embedResponse.setFooter(botModule.embedFooter, botModule.embedFooterThumbnailURL);
     } else {
-      embedResponse.setFooter(botModule.embedFooter); 
+      embedResponse.setFooter(botModule.embedFooter);
     }
   }
 
@@ -74,26 +74,35 @@ async function buildEmbedResponse (message, botModule) {
 }
 
 // Returns the name actually displayed in the server for the user
-async function getAuthorDisplayName (msg) {
-  const member = await msg.guild.members.fetch(msg.author);
-  return member.nickname ? member.nickname : msg.author.username;
+async function getAuthorDisplayName(msg) {
+  // In the event that the user is no longer a member of the channel
+  // an error will be thrown when attempting to determine their server display name. 
+  // This is gauranteed to happen for modules such as the leave or banned announcement. 
+  // The choice is to fail gracefully and revert to sending the username in it's place
+  try {
+    const member = await msg.guild.members.fetch(msg.author);
+    return member.nickname ? member.nickname : msg.author.username;
+  } catch (err) {
+    return msg.author.username;
+  }
+  
 }
 
-async function messageParser (message, botModuleResponse) {
+async function messageParser(message, botModuleResponse) {
   let parsedMessage = botModuleResponse;
 
   parsedMessage = parsedMessage
-                    .replace(/{user}/g, message.author.username)
-                    .replace(/{user_display}/g, await getAuthorDisplayName(message))
-                    .replace(/{channel}/g, message.channel.name);
+    .replace(/{user}/g, message.author.username)
+    .replace(/{user_display}/g, await getAuthorDisplayName(message))
+    .replace(/{channel}/g, message.channel.name);
 
   return parsedMessage;
 }
 
 // Returns true if message was authored by a user that is 
 // assigned a role that's also included in the rolesArray array
-async function roleMatch (message, rolesArray) {
-  for (let i=0; i < rolesArray.length; i++) {
+async function roleMatch(message, rolesArray) {
+  for (let i = 0; i < rolesArray.length; i++) {
     if (message.member.roles.cache.find(r => r.name.toLowerCase() === rolesArray[i])) {
       return true;
     }
