2b45cb10d8348d096af19feb90ae664bff326217
Joseph Greene <josephadamgreene@gmail.com>
2021-10-10 02:13:42 -0400

    Fix bug that invoked both leave and ban events when member was banned

--- discordBot/memberEventModules/leave.js
+++ discordBot/memberEventModules/leave.js
@@ -4,10 +4,21 @@ const {buildAnnouncement} = require("../commandUtils");
 module.exports = {
 	type: 'leave',
 	async execute(member, botId) {
-
     const bot = await Bot.findById(botId);
     const memberServer = member.guild.id;
 
+    const fetchedLogs = await member.guild.fetchAuditLogs({limit: 1,});
+    const lastLog = fetchedLogs.entries.first();
+
+    // The last log exists and it targeted the member that left
+    if (lastLog && lastLog.target.id === member.id) {
+      if(lastLog.action === "MEMBER_BAN_ADD") {
+        // If log suggests the member was banned, then exit this module
+        // as the banned module will do the rest
+        return;
+      }
+    }
+    
     // Find announcement that matches serverId from member and type "leave"
     const announcement = bot.announcementModules.find((announcement) => {
       return (announcement.type === "leave" && announcement.responseChannel.serverId === memberServer);
