4cdf844bc7574f52abf137aa0ffb59fd0737243d
Joseph Greene <josephadamgreene@gmail.com>
2021-12-14 02:49:16 -0500

    allow anyone if allowedRoles array is blank

--- discordBot/commandModules/ping.js
+++ discordBot/commandModules/ping.js
@@ -13,8 +13,9 @@ module.exports = {
     const module = bot.moderationModules.find((module) => String(module._id) === String(moduleId));
     if (!module || !module.enabled) return;
 
-    // Do nothing if command sent by a member without allowed role
-    const roleMatched = roleMatch(message, module.allowedRoles);
+    // Do nothing if command sent by a member without allowed role.
+    // If allowedRoles is blank, then all users are allowed to use command
+    const roleMatched = ((module.allowedRoles.length > 0) ? roleMatch(message, module.allowedRoles) : true);
     if (!roleMatched) {
       try {
         await message.reply("You don't possess an authorized role to use this command.");
