db3f2bc1f834e931fe20e8e19e463b44533cac15
Joseph Greene <josephadamgreene@gmail.com>
2021-12-17 18:06:28 -0500

    fix missing auth middleware error

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const auth = require("../middleware/auth");
 const { User } = require("../models/user");
 const {returnAvatarUrl, returnStatus, returnRoles} = require("../discordBot/botClientUtils");
 
